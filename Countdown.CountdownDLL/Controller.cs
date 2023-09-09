ab3bcee16f13e96ea0bf75c4a415d9b0f6d42537
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-09-10 00:04:20 +0300

    Добавьте файлы проекта.

---
+++ Countdown.CountdownDLL/Controller.cs
@@ -0,0 +1,16 @@
+﻿namespace CountdownDLL
+{
+    public class Controller
+    {
+        public void Execute()
+        {
+            int i = 100;
+            while (i > 0)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+                i--;
+            }
+        }
+    }
+}
---
+++ Countdown/Program.cs
@@ -0,0 +1,13 @@
+﻿using CountdownDLL;
+
+namespace Countdown
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = new Controller();
+            start.Execute();
+        }
+    }
+}
